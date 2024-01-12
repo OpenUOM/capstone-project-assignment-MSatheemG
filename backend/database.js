64d88442cfe75a3138cc2fed9bc03052d9f9ffc1
MSatheemG <156359561+MSatheemG@users.noreply.github.com>
2024-01-12 12:22:48 +0530

    server running

--- backend/database.js
+++ backend/database.js
@@ -106,7 +106,7 @@ const readStudents = async () => {
     });
 }
 
-const readStudeInfo = async (id) => {
+const readStudentInfo = async (id) => {
     const sql = `SELECT * FROM student WHERE id = ?`
     return new Promise((resolve, reject) => {
         knex_db
